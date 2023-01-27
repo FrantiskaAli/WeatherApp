9ba64eefdfdc467ac20d4e1ad9e2fbe587379542
FrantiskaAli <tiskarechk@gmail.com>
2023-01-27 22:15:50 +0000

    in this commit, I added event listener on search button,it calls function for displaying the weather data and also creates buttons of search history. Also I added event listeners on the history buttons as well for easy access

--- script.js
+++ script.js
@@ -5,14 +5,37 @@ const todayWeather = $('#today');
 const searchCity = $('#search-input');
 const forecastRow = $('#forecast');
 const searchBtn = $('#search-button');
+const oldBtns = $('#history');
 //
 
 
 
 
-//http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={API key}   => RESULT[0].LAT and RESULT.[0].lon
 
 //CREATING BUTTONS
+searchBtn.on('click', function(event){
+    event.preventDefault();
+let city = searchCity.val();
+cityToGeo(city)
+let oldSearch = $('<button>')
+oldSearch.text(city).attr('id',city).addClass('.historyBtn')
+oldBtns.append(oldSearch)
+searchCity.val('')
+})
+oldBtns.on('click', 'button', function(event){
+    event.preventDefault();
+ let city = $(this);
+ cityToGeo(city.attr('id'));
+ 
+ 
+ //cityToGeo(city)
+    
+    //cityToGeo(city)
+})
+
+
+
+
 
 
 //START WITH ACTUAL WEATHER
@@ -22,9 +45,9 @@ const searchBtn = $('#search-button');
 //function for current weather
 //thi8s function console.log latitutede and lantitude of the chosen city
 
-let position = []
+
 //this function finds latitude and longtitude of the searched city and returns array with them
-function cityToLat(city){
+function cityToGeo(city){
     let cityURL = 'http://api.openweathermap.org/geo/1.0/direct?q='+ city + '&limit=5&appid=' + myApiKey;
 $.ajax({
     url: cityURL,
@@ -32,7 +55,10 @@ $.ajax({
 }).then(function(response) {
 let latitude = response[0].lat;
 let longtitude = response[0].lon;
-position.push(latitude, longtitude)
+
+displayCurrentWeather(latitude, longtitude);//this function displays current weather
+weatherForecast(latitude, longtitude);//this function displays forecast
+
 })
 
 }
@@ -46,11 +72,12 @@ $.ajax({
     method: 'get'
 }).then(function(response){
     console.log(response)
-    
+    todayWeather.empty()
 //HEADING HTML ELEMENTS 
     let today = moment().format("dddd, MMM DD YYYY");
     let heading = $(`<h2>`);
-    heading.text( `${today} in London `);
+    let placeName = response.name
+    heading.text( `${today} in ${placeName} `);
     //VARIABLES TO ACCESS DATA IN OBJECT RETRIEVED
     let temp = response.main.temp;
     temp -= 273.15;
@@ -84,8 +111,14 @@ $.ajax({
     method:'get'
 }).then(function (response){
     //in this forloop i am attempting to set index as well as 
+    let forecastDay = moment()//making sure i have 5 days forecast headings
+    let y = 0 
+    forecastRow.empty()
 for (let i = 3; i < 40; i += 8 ){
     //creating HTML elements
+    
+    y +=1 //this will increase date (together with line bellow)
+    forecastDay = moment().add(y, 'day').format('DD/MM/YYYY')
     let iconID = response.list[i].weather[0].icon
     let fcIconSource = 'http://openweathermap.org/img/wn/' + iconID + '@2x.png'
     let fcIcon = $('<img>');
@@ -100,7 +133,9 @@ for (let i = 3; i < 40; i += 8 ){
     fcWind /= 1.609344;
     fcWind = fcWind.toFixed(2);
     let fcHumidity = response.list[i].main.humidity;
-
+    
+    let fcheading = $('<h3>')
+    fcheading.text(forecastDay)
     let forecastTemp = $('<p>');
     forecastTemp.text(`Temp: ${fcTemp}°C`); 
     let forecastWind = $('<p>');
@@ -108,7 +143,7 @@ for (let i = 3; i < 40; i += 8 ){
     let forecastHum = $('<p>');
     forecastHum.text(`Humidity: ${fcHumidity}%.`)
    
-    forecastBox.append(fcIcon, forecastTemp, forecastWind, forecastHum)
+    forecastBox.append(fcIcon, fcheading,  forecastTemp, forecastWind, forecastHum)
     forecastRow.append(forecastBox)
 
 
