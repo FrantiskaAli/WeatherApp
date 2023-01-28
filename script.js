f921b3c6039a82524d387d58a257cbdab7a02c36
FrantiskaAli <tiskarechk@gmail.com>
2023-01-28 23:27:06 +0000

    added final comments and deleted useless pieces of code , so my app is ready to deploy

--- script.js
+++ script.js
@@ -8,121 +8,82 @@ const searchBtn = $('#search-button');
 const oldBtns = $('#history');
 const clear = $('#search-clear');
 
-
-
-
-//
-
-clear.on('click', function(event){
-    event.preventDefault;
-    localStorage.clear()
-
-
-})
-
-
-//array oif buttons
-
+//getting localy stored history
 let searchHistory = JSON.parse(localStorage.getItem("history")) || [];
 console.log(searchHistory)
 
+//function for clearing locally stored information on red button click
+clear.on('click', function(event){
+    event.preventDefault;
+    localStorage.clear()
+})
+
+//creating array of buttons using history
 searchHistory.forEach(createButton)
 
-//CREATING BUTTONS
+//On click, event when searched city
 searchBtn.on('click', function(event){
     event.preventDefault();
 let city = searchCity.val();
-let cityURL = 'http://api.openweathermap.org/geo/1.0/direct?q='+ city + '&limit=5&appid=' + myApiKey;
+let cityURL = 'http://api.openweathermap.org/geo/1.0/direct?q='+ city + '&limit=5&appid=' + myApiKey;//create valid url
 $.ajax({
     url: cityURL,
     method: 'get'
 }).then(function(response) {
-if(response.length === 0){
+if(response.length === 0){   //if api call is not responded due to incorrect input stop the function
     alert('not valid input')
     location.reload
     return 
-}else{
-searchHistory.push(city)
-localStorage.setItem('history', JSON.stringify(searchHistory))
-let oldSearch = $('<button>')
-oldSearch.text(city).attr('id',city).attr('style','width: 80%;').addClass('historyBtn btn-dark py-2 my-1 rounded')
-oldBtns.append(oldSearch)
-searchCity.val('')
-
+}else{  //input was correct handling
+searchHistory.push(city) ;
+localStorage.setItem('history', JSON.stringify(searchHistory));//updating local storage
+let oldSearch = $('<button>');//creating history buttons
+oldSearch.text(city).attr('id',city).attr('style','width: 80%;').addClass('historyBtn btn-dark py-2 my-1 rounded');//styling with bootstrap and setting id sttribute to know the city names
+oldBtns.append(oldSearch);
+searchCity.val('');//reset search
+//these variables to be used when executing functions of display weather and weather forecast
 let latitude = response[0].lat;
 let longtitude = response[0].lon;
-
-
 
 displayCurrentWeather(latitude, longtitude);//this function displays current weather
 weatherForecast(latitude, longtitude);//this function displays forecast
 }
 })
-
-
-
-
 })
+//here what happens when old button clicked
 oldBtns.on('click', 'button', function(event){
     event.preventDefault();
  let city = $(this);
- cityToGeo(city.attr('id'));
-
-
-
-
- //cityToGeo(city)
-    
-    //cityToGeo(city)
+ displayOld(city.attr('id'));//display old function
 })
 
 
-
-
-
-//START WITH ACTUAL WEATHER
-
-//FORECAST
-
-//function for current weather
-//thi8s function console.log latitutede and lantitude of the chosen city
-
-
-//this function finds latitude and longtitude of the searched city and returns array with them
-function cityToGeo(city){
+//this function starts with finding latitude and longtitude of the searched city and then executes functions with them as parameters
+function displayOld(city){
     let cityURL = 'http://api.openweathermap.org/geo/1.0/direct?q='+ city + '&limit=5&appid=' + myApiKey;
 $.ajax({
     url: cityURL,
     method: 'get'
 }).then(function(response) {
-
-
 let latitude = response[0].lat;
 let longtitude = response[0].lon;
-
-
-
 displayCurrentWeather(latitude, longtitude);//this function displays current weather
 weatherForecast(latitude, longtitude);//this function displays forecast
-
 })
-
 }
-
 
 //this function wwill call on weather appi and create html alements to display current weather
 function displayCurrentWeather(lat, lon){
     let currentWeatherURL = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat +'&lon=' + lon +'&appid=' + myApiKey;
-$.ajax({
+$.ajax({//calling api
     url: currentWeatherURL,
     method: 'get'
 }).then(function(response){
-    console.log(response) 
     todayWeather.empty()
 //HEADING HTML ELEMENTS 
-    let today = moment().format("dddd, MMM DD YYYY");
+    let today = moment().format("dddd, MMM DD YYYY");//heading made by using moment.js
     let heading = $(`<h2>`);
-    heading.addClass('display-4')
+    heading.addClass('display-4');//bootstrap styling
     let placeName = response.name
     heading.text( `${today} in ${placeName} `);
     //VARIABLES TO ACCESS DATA IN OBJECT RETRIEVED
@@ -145,38 +106,34 @@ $.ajax({
     windSpeed.addClass('.lead').text(`Current speed of wind is ${wind}MPH.`);
     let humidityPercent = $('<p>');
     humidityPercent.addClass('.lead').text(`Humidity is currently ${humidity}%.`);
-   
     heading.append(image)
     todayWeather.append(heading, temperature, windSpeed, humidityPercent )
     todayWeather.addClass('border border-color-danger rounded bg-white')
-
 })
 }
-
+//function that displays forecast blocks
 function weatherForecast(lat, lon){
     let forecastURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat +'&lon=' + lon +'&appid=' + myApiKey;
 $.ajax({
     url: forecastURL,
     method:'get'
 }).then(function (response){
-    //in this forloop i am attempting to set index as well as 
     let forecastDay = moment()//making sure i have 5 days forecast headings
-    let y = 0 
-    forecastRow.empty()
-    let headingMain = $('<h2>');
-    headingMain.text('Five days forecast : ').addClass('w-100 mb-2')
+    let y = 0 //this is variable I am using to increse the date in moment js inside the loloop later on
+    forecastRow.empty()//clearing up old
+    let headingMain = $('<h2>');//creating heading for the forcast row
+    headingMain.text('Five days forecast : ').addClass('w-100 mb-2')//heading width was set to 100% in bootstrap so it doesnt mess around
     forecastRow.append(headingMain)
 for (let i = 3; i < 40; i += 8 ){
+    //index increasing by 8, because in the response object there is information for 5 days for every 3 hours, 8*3= 24 so it adds up to a day
     //creating HTML elements
-
     y +=1 //this will increase date (together with line bellow)
     forecastDay = moment().add(y, 'day').format('DD/MM/YYYY')
     let iconID = response.list[i].weather[0].icon
-    let fcIconSource = 'http://openweathermap.org/img/wn/' + iconID + '@2x.png'
+    let fcIconSource = 'http://openweathermap.org/img/wn/' + iconID + '@2x.png' //creating url for icon based on in-object information
     let fcIcon = $('<img>');
-    fcIcon.attr('src',fcIconSource);
+    fcIcon.attr('src',fcIconSource);//adding the newly made url as src attribute in image
     let forecastBox = $('<article>')
-   
     //accessing values form the object
     let fcTemp = response.list[i].main.temp;
     fcTemp -= 273.15;
@@ -186,8 +143,7 @@ for (let i = 3; i < 40; i += 8 ){
     fcWind = fcWind.toFixed(2);
     let fcHumidity = response.list[i].main.humidity;
     
-  
-
+//creating html elements
     let fcheading = $('<h3>')
     fcheading.text(forecastDay)
     let forecastTemp = $('<p>');
@@ -196,37 +152,16 @@ for (let i = 3; i < 40; i += 8 ){
     forecastWind.text(`Wind: ${fcWind}MPH`);
     let forecastHum = $('<p>');
     forecastHum.text(`Humidity: ${fcHumidity}%.`)
-   
     forecastBox.addClass('bg-info m-1 p-3 rounded text-dark border border-dark col-lg-2 col-sm-4 col-md-3 col-10')
     forecastBox.append(fcIcon, fcheading,  forecastTemp, forecastWind, forecastHum)
     forecastRow.append(forecastBox)
-
-
 }
-
 })
-
-
-
 }
-
 //function to create buttons
-function createButton(cityname){
+function createButton(cityname){//this function creates buttons simple way, it is used at the begining when retrieving old data
     let historySearch = $('<button>')
 historySearch.text(cityname).attr('id',cityname).attr('style','width: 80%;').addClass('historyBtn btn-dark py-2 my-1 rounded')
 oldBtns.append(historySearch)
 }
 
-//i created this function to ensure only cities that exists are searched and saved in as buttons
-function checkValidity(input){
- let cityURL = 'http://api.openweathermap.org/geo/1.0/direct?q='+ input + '&limit=5&appid=' + myApiKey;
- $.ajax({
-    url: cityURL,
-    method: 'get'
-}).then(function(response) {
-    console.log(response)
-    if(response.length === 0){
-        alert('not valid input')
-    }
-
-})}
